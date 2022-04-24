6eb716c203bde560168ec28a6667befe559e08d1
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-04-24 19:38:05 +0300

    added the display account and find account class decorator to the credential class

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 from re import T
 
+from requests import delete
 
 class User:
     '''
@@ -13,30 +14,27 @@ class User:
         self.user_name = user_name
         self.password = password
         
-        def save_user(self):
+    def save_user(self):
             """
             save a new user object in our user list
             """
-
-            
-
             User.user_list.append(self)  #add new users to the user list
         
-        def delete_user(self):
+    def delete_user(self):
             """
             deletes a saved user from the list
             """
             User.user_list.remove(self)
 
-        @classmethod
-        def display_users(cls):
+    @classmethod
+    def display_users(cls):
             """
             return information from the users list
             """
             return cls.user_list
 
-        @classmethod
-        def find_by_number(cls,number):
+    @classmethod
+    def find_by_number(cls,number):
             """
             Method that takes in a username and returns a user that matches that number
             """
@@ -44,8 +42,8 @@ class User:
                 if user.user_name == number:
                     return user
 
-        @classmethod
-        def user_exist(cls,number):
+    @classmethod
+    def user_exist(cls,number):
             for user in cls.user_list:
                 if user.user_name == number:
                     return True
@@ -63,4 +61,38 @@ class Credentials:
         """
         helps define properties of our object self
         """
-        
+
+        self.account_username = account_username
+        self.account_name = account_name
+        self.account_password = account_password
+
+    def save_account(self):
+        """
+        save user information into the accounts
+        """
+
+        Credentials.accounts.append(self)
+
+    def delete_account(self):
+        """
+        delete a saved credential from ana account
+        """
+        Credentials.accounts.remove(self)
+
+    @classmethod
+    def display_accounts(cls):
+            """
+            return information from the users list
+            """
+            return cls.accounts
+
+    @classmethod
+    def find_by_number(cls,number):
+            """
+            Method that takes in a username and returns a user that matches that number
+            """
+            for account in cls.accounts:
+                if account.account_username == number:
+                    return account
+
+
