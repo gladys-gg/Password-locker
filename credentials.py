2a2cfff6c02ff28f48a6110d8769b93e170cd978
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-14 14:29:41 +0300

    updated the credential class

--- credentials.py
+++ credentials.py
@@ -5,5 +5,64 @@ class Credentials:
     generates new instances of credentials
     """
                     
-    accounts = []  #list of accounts a user may have
+    credentials_list = []  #list of accounts a user may have
     
+
+    def __init__(self, account,account_username,account_password):
+        """
+        helps define properties of our object self
+        """
+
+        self.account = account
+        self.account_username = account_username
+        self.account_password = account_password
+    
+    def save_credential(self):
+
+        '''
+        save_account method saves account objects into credentials_list
+        '''
+
+        Credentials.credentials_list.append(self)
+
+
+    def delete_credential(self):
+
+        '''
+        delete_account method deletes a saved account from the credentials_list
+        '''
+
+        Credentials.credentials_list.remove(self)
+        
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the account list
+        '''
+        return cls.credentials_list
+
+    @classmethod
+    def find_credential(cls,account):
+            """
+            Method that takes in a username and returns a user that matches that number
+            """
+            for credential in cls.credentials_list:
+                if credential.account== account:
+                    return credential
+                
+    @classmethod
+    def copy_credentials(cls,account):
+        
+        """
+        a method that copies credentials info
+        """	
+        found_credentials = Credentials.find_credential(account)
+        pyperclip.copy(found_credentials.password)
+        
+    @classmethod
+    def credentials_exist(cls, account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+            return False
